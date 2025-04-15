ca0464b0b5afb767aa16179a62ba90799b21ca15
dongdorrong <hun5879@naver.com>
2025-04-15 12:35:39 +0900

    feat: Terraform 초기 코드 추가 (apply X)

---
+++ project03/terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "Public Subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "Private Subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "nat_gateway_ip" {
+  description = "NAT Gateway Public IP"
+  value       = aws_eip.nat.public_ip
+} 
