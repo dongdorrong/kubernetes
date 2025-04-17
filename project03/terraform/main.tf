bc83be207c4e5b79263497b0758a20e02f719719
dongdorrong <hun5879@naver.com>
2025-04-17 09:39:54 +0900

    refactor: provider 파일로 분리

--- project03/terraform/main.tf
+++ project03/terraform/main.tf
@@ -12,17 +12,3 @@ terraform {
     path = "./tfstate/terraform.tfstate"
   }
 }
-
-# AWS Provider 설정
-provider "aws" {
-  region  = "ap-northeast-2"
-  profile = "private"
-  alias   = "private"  # private 프로필용 alias 설정
-
-  # 프로필 사용 확인을 위한 기본 태그 설정
-  default_tags {
-    tags = {
-      ManagedBy = "terraform"
-    }
-  }
-} 
