8010ca824283b4c78d9281294771a525a06828d7
dongdorrong <hun5879@naver.com>
2025-09-27 18:36:05 +0900

    feat: project06 initial commit

---
+++ project06/terraform/eks_addon.tf
@@ -0,0 +1,194 @@
+# kube-proxy 애드온
+resource "aws_eks_addon" "kube_proxy" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "kube-proxy"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [aws_eks_cluster.this]
+}
+
+# CoreDNS 애드온
+resource "aws_eks_addon" "coredns" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "coredns"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [aws_eks_cluster.this]
+}
+
+# EKS Pod Identity Agent 애드온
+resource "aws_eks_addon" "pod_identity" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "eks-pod-identity-agent"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.coredns,
+    ]
+}
+
+# VPC CNI 애드온
+resource "aws_eks_addon" "vpc_cni" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "vpc-cni"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.kube_proxy,
+    ]
+}
+
+# EBS CSI 드라이버 애드온
+resource "aws_eks_addon" "ebs_csi" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "aws-ebs-csi-driver"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.coredns,
+    ]
+}
+
+# Metrics Server 애드온
+resource "aws_eks_addon" "metrics_server" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "metrics-server"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [aws_eks_cluster.this]
+}
+
+# AWS Network Flow Monitor 애드온
+resource "aws_eks_addon" "network_flow_monitor" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "aws-network-flow-monitoring-agent"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.pod_identity,
+    ]
+}
+
+# 노드 모니터링 애드온
+resource "aws_eks_addon" "node_monitoring" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "eks-node-monitoring-agent"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.pod_identity,
+    ]
+}
+
+# CSI 스냅샷 컨트롤러 애드온
+resource "aws_eks_addon" "snapshot_controller" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "snapshot-controller"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [aws_eks_cluster.this]
+}
+
+# AWS Private CA Connector for Kubernetes 애드온
+resource "aws_eks_addon" "privateca_connector" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "aws-privateca-connector-for-kubernetes"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.pod_identity,
+    ]
+}
+
+# Mountpoint for Amazon S3 CSI 드라이버 애드온
+resource "aws_eks_addon" "mountpoint_s3_csi" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "aws-mountpoint-s3-csi-driver"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.pod_identity,
+    ]
+}
+
+# Amazon EFS CSI 드라이버 애드온
+resource "aws_eks_addon" "efs_csi" {
+    cluster_name                = aws_eks_cluster.this.name
+    addon_name                  = "aws-efs-csi-driver"
+    resolve_conflicts_on_create = "OVERWRITE"
+    resolve_conflicts_on_update = "PRESERVE"
+
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.pod_identity,
+    ]
+}
+
+# AWS Load Balancer Controller
+# - https://docs.aws.amazon.com/ko_kr/eks/latest/userguide/lbc-helm.html
+resource "helm_release" "aws_load_balancer_controller" {
+    name       = "aws-load-balancer-controller"
+    repository = "https://aws.github.io/eks-charts"
+    chart      = "aws-load-balancer-controller"
+    namespace  = "kube-system"
+
+    set {
+      name  = "region"
+      value = data.aws_region.current.name
+    }
+
+    set {
+      name  = "vpcId"
+      value = aws_vpc.main.id
+    }
+
+    set {
+      name  = "clusterName"
+      value = aws_eks_cluster.this.name
+    }
+
+    set {
+      name  = "serviceAccount.create"
+      value = "false"
+    }
+
+    set {
+      name  = "serviceAccount.name"
+      value = kubernetes_service_account.aws_load_balancer_controller.metadata[0].name
+    }
+
+    depends_on = [
+      aws_iam_policy.aws_load_balancer_controller,
+      aws_iam_role.aws_load_balancer_controller,
+      aws_iam_role_policy_attachment.aws_load_balancer_controller,
+      kubernetes_service_account.aws_load_balancer_controller,
+      aws_eks_pod_identity_association.aws_load_balancer_controller,
+    ]
+}
+
+# StorageClass 생성
+resource "kubernetes_manifest" "storageclass" {
+    manifest = yamldecode(file("${path.module}/manifests/storageclass.yaml"))
+    depends_on = [
+      aws_eks_cluster.this,
+      aws_eks_addon.ebs_csi,
+    ]
+}
---
+++ project06/terraform/iam_assume_role.tf
@@ -0,0 +1,85 @@
+# # 테라폼 관리용 역할 (기존 역할을 테라폼으로 관리하기 위한 정의)
+# resource "aws_iam_role" "terraform_admin" {
+#     name                 = "terraform-assume-role"
+#     # name                 = "${local.project_name}-terraform-assume-role"
+#     max_session_duration = 43200  # 12시간
+#     assume_role_policy   = jsonencode({
+#         Version = "2012-10-17"
+#         Statement = [
+#             {
+#                 Effect = "Allow"
+#                 Principal = {
+#                     AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+#                 }
+#                 Action = "sts:AssumeRole"
+#                 Condition = {}
+#             }
+#         ]
+#     })
+# }
+
+# # 테라폼 역할에 관리자 권한 부여
+# resource "aws_iam_role_policy_attachment" "terraform_admin" {
+#     role       = aws_iam_role.terraform_admin.name
+#     policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
+
+# # EKS 관리자 IAM 역할
+# resource "aws_iam_role" "eks_admin" {
+#     name                 = "eks-assume-role"
+#     # name                 = "${local.project_name}-eks-assume-role"
+#     max_session_duration = 43200  # 12시간
+#     assume_role_policy   = jsonencode({
+#         Version = "2012-10-17"
+#         Statement = [
+#             {
+#                 Effect = "Allow"
+#                 Principal = {
+#                     AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+#                 }
+#                 Action = "sts:AssumeRole"
+#             }
+#         ]
+#     })
+# }
+
+# # EKS 관리자 정책
+# resource "aws_iam_role_policy" "eks_admin" {
+#     name = "eks-admin-policy"
+#     role = aws_iam_role.eks_admin.id
+
+#     policy = jsonencode({
+#         Version = "2012-10-17"
+#         Statement = [
+#             {
+#                 Effect = "Allow"
+#                 Action = [
+#                     "eks:DescribeCluster"    # kubeconfig 생성 및 클러스터 접근에 필요
+#                 ]
+#                 Resource = "*"
+#             }
+#         ]
+#     })
+# }
+
+# # EKS 역할에 관리자 권한 부여
+# resource "aws_iam_role_policy_attachment" "eks_admin" {
+#     role       = aws_iam_role.eks_admin.name
+#     policy_arn = aws_iam_role_policy.eks_admin
+# }
+
+# AWS 계정 ID를 가져오기 위한 데이터 소스
+data "aws_caller_identity" "current" {}
+
+# AWS 리전 정보를 가져오기 위한 데이터 소스  
+data "aws_region" "current" {}
+
+# EKS 관리자 AssumeRole에 대하여 data 리소스 선언
+data "aws_iam_role" "eks_admin" {
+    name = "eks-assume-role"
+}
+
+# Terraform 관리자 AssumeRole에 대하여 data 리소스 선언
+data "aws_iam_role" "terraform_admin" {
+    name = "terraform-assume-role"
+}
