2750569020685b6adae819b2a733ef87df6dbde3
dongdorrong <62313586+dongdorrong@users.noreply.github.com>
2025-09-27 13:40:08 +0900

    refactor: manage eks access with access entries

---
+++ project05/terraform/eks_access_entries.tf
@@ -0,0 +1,54 @@
+# EKS Access Entry - Terraform 관리자 역할
+resource "aws_eks_access_entry" "terraform_admin" {
+    cluster_name  = aws_eks_cluster.this.name
+    principal_arn = data.aws_iam_role.terraform_admin.arn
+}
+
+resource "aws_eks_access_policy_association" "terraform_admin_cluster_admin" {
+    cluster_name  = aws_eks_cluster.this.name
+    principal_arn = data.aws_iam_role.terraform_admin.arn
+    policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+    access_scope {
+        type = "cluster"
+    }
+
+    depends_on = [aws_eks_access_entry.terraform_admin]
+}
+
+# EKS Access Entry - EKS 관리자 역할
+resource "aws_eks_access_entry" "eks_admin" {
+    cluster_name  = aws_eks_cluster.this.name
+    principal_arn = data.aws_iam_role.eks_admin.arn
+}
+
+resource "aws_eks_access_policy_association" "eks_admin_cluster_admin" {
+    cluster_name  = aws_eks_cluster.this.name
+    principal_arn = data.aws_iam_role.eks_admin.arn
+    policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+    access_scope {
+        type = "cluster"
+    }
+
+    depends_on = [aws_eks_access_entry.eks_admin]
+}
+
+# EKS Access Entry - 기본 노드 그룹 역할
+resource "aws_eks_access_entry" "default_node_group" {
+    cluster_name  = aws_eks_cluster.this.name
+    principal_arn = aws_iam_role.default_node_group.arn
+    type          = "EC2_LINUX"
+}
+
+resource "aws_eks_access_policy_association" "default_node_group" {
+    cluster_name  = aws_eks_cluster.this.name
+    principal_arn = aws_iam_role.default_node_group.arn
+    policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSNodeAccessPolicy"
+
+    access_scope {
+        type = "cluster"
+    }
+
+    depends_on = [aws_eks_access_entry.default_node_group]
+}
